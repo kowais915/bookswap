bb07f8f7822108a3438dd84b697f4bc29c93729a
kowais915 <kowais915@gmail.com>
2023-06-27 18:49:05 +0500

    :sparkles: Added a navbar
    woring on the hero section

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+
+
+const Hero = () => {
+    return ( 
+
+        <div className="hero">
+            <h1>This is the hero section</h1>
+        </div>
+     );
+}
+ 
+export default Hero;
---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Navbar.module.css'
+
+const Navbar = () => {
+    return ( 
+    <>
+    <div className={styles.navbar}>
+
+        <h1>Navbar</h1>
+       
+    </div>
+       
+    
+    </> );
+}
+ 
+export default Navbar;
