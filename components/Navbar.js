c8a10eb5d7a129e53d283f63eac2aff95747ed8d
kowais915 <kowais915@gmail.com>
2023-06-27 19:35:43 +0500

    :lipstick: Styled the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,26 @@
 import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
     return ( 
     <>
     <div className={styles.navbar}>
 
-        <h1>Navbar</h1>
+        <div className={styles.brand}>
+            BookSwap
+        </div>
+
+       <div className={styles.links}>
+       
+            <Link className={styles.navlink} href="/"> Home</Link>
+
+            <Link className={styles.navlink} href="/">About</Link>
+
+            <Link className={styles.navlink} href="/">Contact</Link>
+
+       </div>
+
+
        
     </div>
        
