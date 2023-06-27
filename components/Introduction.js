9a98188b2a05d8cf36ad2c83374e924ee92148ec
kowais915 <kowais915@gmail.com>
2023-06-27 20:00:36 +0500

    :sparkles: Done with the intro section

--- components/Introduction.js
+++ components/Introduction.js
@@ -1,10 +1,20 @@
 
-
+import styles from '../styles/Intro.module.css'
 
 const Introduction = () => {
     return ( 
-        <div className="introduction">
-            <h1>This is the introduction section</h1>
+        <div className={styles.introduction}>
+            <div className={styles.title}>
+                <h1>HOW DOES IT WORK?</h1>
+
+            </div>
+
+            <div className={styles.paragraph}>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis massa feugiat nisi ullamcorper, a varius arcu accumsan. Donec ullamcorper rhoncus felis, at posuere neque efficitur quis. Integer tellus felis, luctus sit amet diam sed, commodo consectetur dolor. Suspendisse commodo ligula vitae venenatis ornare. Quisque vitae facilisis erat, vel pretium nunc. Morbi euismod condimentum ex, ac convallis sapien accumsan eget. Donec bibendum purus a euismod varius. Pellentesque pulvinar, urna sed sollicitudin porta, ligula est fringilla tortor, ut lacinia orci risus sed dolor. In hac habitasse platea dictumst. Proin nec laoreet ex, ac condimentum nunc. Curabitur quis augue nunc. Ut semper justo ut felis eleifend, id pharetra dolor rhoncus. Pellentesque tortor leo, fermentum id viverra in, condimentum eget est. Fusce ac nisl sed nisi lobortis bibendum. Proin mauris nisi, molestie a dapibus ac, pretium sit amet ipsum.
+
+                </p>
+            </div>
         </div>
      );
 }
