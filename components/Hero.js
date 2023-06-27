502f757468e892ad33e93ecc726573190cd965cb
kowais915 <kowais915@gmail.com>
2023-06-27 19:17:44 +0500

    :sparkles: Done with the hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,26 @@
-
+import styles from '../styles/Hero.module.css'
+import Image from 'next/image'
 
 const Hero = () => {
     return ( 
 
-        <div className="hero">
-            <h1>This is the hero section</h1>
+        <div className={styles.hero}>
+            <div className={styles.picture}>
+               <Image
+
+                src='/book.png'
+                width={500}
+                height={700}
+                alt="book"
+
+               />
+            </div>
+
+            <div className="tagline">
+                <p className={styles.tagline}>
+                    Exchange Your Books With Anyone. Everywhere.
+                </p>
+            </div>
         </div>
      );
 }
