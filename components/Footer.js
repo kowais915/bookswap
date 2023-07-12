f0e0a979bfd968c04c1580a2bdc27fce53e0d042
kowais915 <kowais915@gmail.com>
2023-07-13 00:32:10 +0500

    :art: Styled the footer: changed the color

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <h1>This is footer</h1>
+           
         </div>
       );
 }
