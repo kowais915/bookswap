a0803f6fc876003187d559f37442afbcb3b5aacd
kowais915 <kowais915@gmail.com>
2023-07-13 00:59:12 +0500

    :construction: Wrapped the root components with ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import {ThemeProvider} from '../context/ThemeContext';
+import {ThemeContext } from  '../context/ThemeContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+       <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
