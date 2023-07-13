5f7ab6e9b8915ac4a8ba73dd0ec602d65860f991
kowais915 <kowais915@gmail.com>
2023-07-14 03:07:52 +0500

    :construction: Working on the user profile: dashboard

--- components/dashboard/pages/Profile.js
+++ components/dashboard/pages/Profile.js
@@ -1,11 +1,60 @@
+import Image from 'next/image';
+import styles from '../../../styles/Dashboard.module.css'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+
 const Profile = () => {
     return ( 
 
-        <>
+        <div>
 
-            <h3>This is your profile</h3>
+                <div >
+
+                    <Stack direction="row" spacing={10}>
+
+
+                    <Stack  spacing={2}>
+
+                        <Image
+                            src='/user.jpeg'
+                            alt='user image'
+                            width={200}
+                            height={200}
+                            className={styles.image}
+                        
+                        
+                        />  
+
+                        <Typography
+                            align='center'
+                        >
+                            Owais Khan
+                        </Typography>
+
+
+                        
+
+                    </Stack>
+                   
+
+                   
+
+                    <div className="location">
+                        <h3>Loction: Post office Behlola District Charsadda</h3>
+                    </div>
+
+                    </Stack>
+                    
+                        
+
+
+                </div>  
+
+                <div className="info">
+
+                </div>
         
-        </>
+        </div>
      );
 }
  
